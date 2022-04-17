1a0b48921efd82cd60b40104fc4588882843253f
Ravi Singh <ravi4636singh@gmail.com>
2022-04-17 23:32:36 +0530

    Change UI and made responsive

--- script.js
+++ script.js
@@ -1,34 +1,58 @@
-const word = document.querySelector('#word');
-const searchButton = document.querySelector('#search-btn');
-const definition = document.querySelector('.definition');
-const notFound = document.querySelector('.not-found');
-const loading = document.querySelector('#loading');
+const form = document.querySelector('#form')
+const inputText = document.querySelector('#input_text')
+const loadPage = document.querySelector('#load_page')
 
-searchButton.addEventListener('click', () => {
-    const getWord = word.value;
-    
-    if(getWord == ''){
-        alert('Enter a word :)');
-        return;
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let word = inputText.value
+
+    if(word === ''){
+        alert('Please enter a word :)')
+        return
     }
 
-    getDef(getWord);
+    loadPage.style.display = 'none'
+
+    getDef(word)
 })
 
-const getDef = async (getWord) => {
-    loading.style.display = 'block';
-    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${getWord}`);
-    const data = await response.json();
-    loading.style.display = 'none';
-    // console.log(data[0].meanings[0].definitions[0].definition); --> Definition
-    const noResult = data.title;
-    if(typeof noResult == 'string'){
-        notFound.innerText = noResult;
-        notFound.style.display = 'block';
-        return;
+const notFoundBox = document.querySelector('#not_found_box')
+const loading = document.querySelector('#loading')
+
+async function getDef(word){
+    loading.style.display = 'block'
+    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    const data = await res.json()
+
+    notFoundBox.style.display = 'none'
+    loading.style.display = 'none'
+    // If meaning not found
+    if(typeof data.title === 'string'){
+        notFoundBox.style.display = 'block'
+        return
     }
 
-    const def = data[0].meanings[0].definitions[0].definition;
-    definition.innerText = def;
-    definition.style.display = 'block';
+    let audioFile = data[0].phonetics[0].audio
+    let meanings = data[0].meanings.pop().definitions[0]
+    
+    // If example not available
+    if(meanings.example == undefined){
+        meanings.example = 'No Example...'
+    }
+
+    setInfo(audioFile, meanings)
+}
+
+const wordPronunciation = document.querySelector('#word_pronunciation')
+const audioFile = document.querySelector('#audio_file')
+const resultBox = document.querySelector('#result_box')
+const definition = document.querySelector('#definition')
+const example = document.querySelector('#example_box')
+
+function setInfo(audio, meaning){
+    wordPronunciation.style.display = 'block'
+    resultBox.style.display = 'block'
+    audioFile.src = audio
+    definition.innerText = meaning.definition
+    example.innerText = meaning.example
 }
